7f131948a2b3115fdd335af1f73305e4b746ffa6
Rosalee Zhou <zhouyj1114_zz@163.com>
2021-06-12 15:47:16 +0800

    111

--- set2/exercise1.py
+++ set2/exercise1.py
@@ -12,24 +12,24 @@ import platform
 print("hello! Let's get started")  # it printed "hello! Let's get started"
 
 some_words = ['what', 'does', 'this', 'line', 'do', '?']
-
+# I think this will print "what does this line do" by calling the print function. 
 for word in some_words:
-    print(word)
+    print(word)        # it printed "what","does","this","line","do","?"respectively.
+# I think this will print a random word in the list.
+for x in some_words:      
+    print(x)           
 
-for x in some_words:
-    print(x)
-
-print(some_words)
-
+print(some_words)      # it printed "what","does","this","line","do","?"respectively.
+# I think this will print "some_words contains more than 3 words".
 if len(some_words) > 3:
-    print('some_words contains more than 3 words')
-
+    print('some_words contains more than 3 words')      #It printed "some_words contains more than 3 words".
+# I think this will print some details about my laptop system and settings.
 def usefulFunction():
     """
     You may want to look up what uname does before you guess
     what the line below does:
     https://docs.python.org/3/library/platform.html#platform.uname
     """
-    print(platform.uname())
+    print(platform.uname())    #It printed a namedtuple containing system, node, release, version, machine, and processor
 
 usefulFunction()
