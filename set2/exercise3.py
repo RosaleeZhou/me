03c87866b715da3bf4770b009fc359a4ad67d0cd
Rosalee Zhou <zhouyj1114_zz@163.com>
2021-06-12 19:19:06 +0800

    1

--- set2/exercise3.py
+++ set2/exercise3.py
@@ -119,10 +119,10 @@ def loops_3():
     """
 
     a_number_block = []
-    for ji in range(10):
+    for i in range(10):
         a_number_list = []
         for j in range(10):
-            a_number_list.append(str(j))
+            a_number_list.append(str(i))
             a_number_block.append(a_number_list)
     print (a_number_block)
     return a_number_block
